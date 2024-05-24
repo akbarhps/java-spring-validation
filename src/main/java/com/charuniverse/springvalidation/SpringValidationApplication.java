075f8db682a88c7d87e73e88472f17420f863f95
ahps <akbarputrasrg@gmail.com>
2024-05-24 08:20:50 +0700

    course f: configuration properties

--- src/main/java/com/charuniverse/springvalidation/SpringValidationApplication.java
+++ src/main/java/com/charuniverse/springvalidation/SpringValidationApplication.java
@@ -1,9 +1,14 @@
 package com.charuniverse.springvalidation;
 
+import com.charuniverse.springvalidation.properties.DatabaseProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+        DatabaseProperties.class
+})
 public class SpringValidationApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/charuniverse/springvalidation/properties/DatabaseProperties.java
@@ -0,0 +1,22 @@
+package com.charuniverse.springvalidation.properties;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
+
+@Data
+@Validated
+@NoArgsConstructor
+@AllArgsConstructor
+@ConfigurationProperties("database")
+public class DatabaseProperties {
+
+    @NotBlank
+    private String username;
+
+    @NotBlank
+    private String password;
+}
